4339a47b2a6d9fdf041099cbd5e431b143f75574
RasikaPatil742 <rasikapatil742@gmail.com>
2026-03-25 22:54:48 +0530

    Implemented Logistic Regression and Random Forest with SMOTE

--- train_model.py
+++ train_model.py
@@ -1,12 +1,55 @@
 import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import classification_report, confusion_matrix
+from imblearn.over_sampling import SMOTE
+
 
 df = pd.read_excel("credit_card_fraud_10k.csv.xlsx")
-print(df.head())
-print(df['Class'].value_counts())
+df.columns = df.columns.str.strip()
 
-x = df.drop('Class', axis=1)
-y = df['Class']
+# Convert categorical to numeric
+df = pd.get_dummies(df, drop_first=True)
 
-# from sklearn.preprocessing import StandardScaler
-# scaler = StandardScaler()
-# x_scaled = scaler.fit_transform(x)
+print("After encoding:", df.head())
+
+#separating the features and target variable
+x = df.drop('is_fraud', axis=1)
+y = df['is_fraud']
+
+#handling imbalance in the dataset using SMOTE
+smote = SMOTE()
+x_resampled, y_resampled = smote.fit_resample(x, y)
+print("Dataset Resampled Successfully✅")
+
+#train-test split
+x_train, x_test, y_train, y_test = train_test_split(
+    x_resampled, y_resampled, test_size=0.2, random_state=42)
+
+#scaling
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+#logistic regression model
+lr_model = LogisticRegression()
+lr_model.fit(x_train, y_train)
+
+lr_pred = lr_model.predict(x_test)
+print("\nLogistic Regression Results")
+print(confusion_matrix(y_test, lr_pred))
+print(classification_report(y_test, lr_pred))
+
+#random forest model
+rf_model = RandomForestClassifier(n_estimators=100)
+rf_model.fit(x_train, y_train)
+
+rf_pred = rf_model.predict(x_test)
+print("\nRandom Forest Results")
+print(confusion_matrix(y_test, rf_pred))
+print(classification_report(y_test, rf_pred))
